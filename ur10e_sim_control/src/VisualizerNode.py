1de96005ef018cc594442d22b670222dff717c52
juandanielsg <61045853+juandanielsg@users.noreply.github.com>
2023-07-04 14:48:18 +0200

    Add files via upload

--- ur10e_sim_control/src/VisualizerNode.py
+++ ur10e_sim_control/src/VisualizerNode.py
@@ -11,17 +11,19 @@ import numpy as np
 def main():
 
     rospy.init_node("VisualizerNode")
-
+    rate = rospy.Rate(10)
     tool = rospy.get_param('tool')
     controller = visualizationController(visual_subscriber_topic="/current_pose", subscriber_topic="/path_plan", publisher_topic="/visualization_marker_array")
     cntrl = rospy.get_param('controller_type')
     if cntrl != "jointP":
         hitboxController = HitboxGroup(sub_topic="/current_poses", pub_topic="/nothing_2_see_here", visual_pub_topic="/visualization_hitboxes", tool=tool)
 
+    
     while not rospy.is_shutdown():
         
         os.system("clear")
         print(bcolors.OKGREEN + "Visuals rendered properly" + bcolors.ENDC)
+        
         #if cntrl != "jointP":
             #hitboxController.collisionCheck()
             #hitboxController.display()
@@ -29,14 +31,16 @@ def main():
 
         if controller.constraintExists:
             #controller.updateConstraint(np.array(controller.currentPose))
+            #print("IM HERE")
+
             if controller.currentToolPos is not None and controller.currentEEPos is not None:
-
-                print("Current RCM error (mm): " + str(controller.getRCMError()*1000))
-
-
-
-        
-        #rate.sleep()
+                controller.tool_path.append(controller.currentToolPos)
+                controller.tool_path.publish()
+                
+                print(controller.getRCMError() * 1000)
+                controller.rcm_path.publish()
+                
+        rate.sleep()
 
 
 if __name__ == "__main__":
